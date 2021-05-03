39edb06a4e3dcb44d6053ac18bf7644f56c3011c
Joakim <j-pf@live.se>
2021-05-03 10:20:38 +0200

    added structure

---
+++ wp-content/themes/lagomfulahattar/functions.php
@@ -0,0 +1,47 @@
+<?php 
+
+
+function load_styles() {
+    wp_enqueue_style( 'commonStyle', get_template_directory_uri() . './style.css', false );
+//    wp_enqueue_style( 'animations', get_template_directory_uri() . './css/animation.css', false );
+}
+ 
+
+
+ function load_scripts() {
+//    wp_enqueue_script( 'scripts', get_template_directory_uri() . '/js/main.js', array(), '1.0.0', true );
+}  
+
+
+
+
+
+
+//add_action( 'wp_enqueue_scripts', 'load_scripts' );
+add_action( 'wp_enqueue_scripts', 'load_styles' );
+add_theme_support('post-thumbnails');
+add_theme_support('menus');
+add_theme_support('woocommerce');
+
+
+
+
+
+
+
+
+
+
+/**
+ * Proper ob_end_flush() for all levels
+ *
+ * This replaces the WordPress `wp_ob_end_flush_all()` function
+ * with a replacement that doesn't cause PHP notices.
+ */
+remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );
+add_action( 'shutdown', function() {
+   while ( @ob_end_flush() );
+} );
+
+
+?>
---
+++ wp-content/themes/lagomfulahattar/navbar.php
@@ -0,0 +1,10 @@
+
+
+
+   
+    <?php wp_nav_menu( [
+        'link_before' => '<p class="linkText">',
+        'link_after' => '</p><div class="linkFloor"></div>',
+        'container_id' => 'sideNavBar',
+    ]); ?>
+
