716aa04e7f0d956115eb12a08772561df6673463
Victor Barraza <victor.barrazamerino@medieinstitutet.se>
2021-05-04 13:14:30 +0200

    contact
    contact

---
+++ wp-content/themes/lagomfulahattar/page.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>  
+
+<?php get_template_part('navbar'); ?>
+
+<main>
+
+
+<?php
+the_content();
+?>
+
+<?php get_template_part('parallax'); ?>
+
+
+
+
+</div>
+
+</main>
+
+<?php get_footer(); ?>  
