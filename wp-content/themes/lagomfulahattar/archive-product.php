6f0c6fab55dfb5688a745921f3acf95acf067167
Victor Barraza <victor.barrazamerino@medieinstitutet.se>
2021-05-05 14:33:08 +0200

    webbutik
    få ut varor

---
+++ wp-content/themes/lagomfulahattar/archive-product.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>  
+
+
+<?php get_template_part('navbar'); ?>
+
+
+
+
+<main>
+<?php get_template_part('parallax'); ?>
+
+</div>
+
+</main>
+
+
+
+
+
+
+<?php get_footer(); ?>  
