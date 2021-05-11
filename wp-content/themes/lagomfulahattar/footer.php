f04a2a6db5cecbee5e6fd6758a36317fe3265e87
Joakim <j-pf@live.se>
2021-05-11 20:43:20 +0200

    structured-css-and-buggfix

--- wp-content/themes/lagomfulahattar/footer.php
+++ wp-content/themes/lagomfulahattar/footer.php
@@ -3,9 +3,19 @@
     <footer>
 
         <div class="footerTop">
-        <?php dynamic_sidebar("footer_top"); ?>
+            <div class="footerTopLeft">
+                <?php dynamic_sidebar("footer_top_left"); ?>
+                
+            </div>
+            <div class="footerTopRight">
+                <?php dynamic_sidebar("footer_top_right"); ?>
+                
+            </div>
         </div>
         <div class="footerBottom">
+            
+        <?php dynamic_sidebar("footer_social_menu"); ?>
+
 
             <div class="footerLogoWrapper">
                 <?php dynamic_sidebar("logo"); ?>
--- wp-content/themes/lagomfulahattar/functions.php
+++ wp-content/themes/lagomfulahattar/functions.php
@@ -1,24 +1,64 @@
 <?php
 
+use DeliciousBrains\WPMDB\Free\UI\Template;
 
-function load_styles()
+
+function load_styles_and_scripts()
 {
-    wp_enqueue_style('animations', get_template_directory_uri() . '/CSS/animation.css', false);
-    wp_enqueue_style('styleForMyAcc', get_template_directory_uri() . '/CSS/myacc.css', false);
-    wp_enqueue_style('commonStyle', get_template_directory_uri() . '/style.css', false);
-    wp_enqueue_style('homeCss', get_template_directory_uri() . '/CSS/home.css', false);
-    wp_enqueue_style('singleCss', get_template_directory_uri() . '/CSS/single.css', false);
-    wp_enqueue_style('butiker', get_template_directory_uri() . '/CSS/butiker.css', false);
-    wp_enqueue_style( 'kontakta-oss', get_template_directory_uri() . '/CSS/kontakta-oss.css', false );
-    wp_enqueue_style('policy', get_template_directory_uri() . '/CSS/policy.css', false);
-    wp_enqueue_style('varukorg', get_template_directory_uri() . '/CSS/varukorg.css', false);
-    wp_enqueue_style('widgetsAndIcons', get_template_directory_uri() . '/CSS/widgetsAndIcons.css', false);
-    wp_enqueue_style( 'single-product', get_template_directory_uri() . '/CSS/single-product.css', false );
-    wp_enqueue_style( 'archive-product', get_template_directory_uri() . '/CSS/archive-product.css', false );
+    global $template;
 
-    wp_enqueue_style('kassaCss', get_template_directory_uri() . '/CSS/kassa.css', false);
+    wp_register_style('indexStyle', get_template_directory_uri() . '/CSS/index.css', array(), "", false);
+    wp_register_style('animations', get_template_directory_uri() . '/CSS/animation.css', array(), "", false);
+    wp_register_style('styleForMyAcc', get_template_directory_uri() . '/CSS/myacc.css', array(), "", false);
+    wp_register_style('commonStyle', get_template_directory_uri() . '/style.css', array(), "", false);
+    wp_register_style('homeCss', get_template_directory_uri() . '/CSS/home.css', array(), "", false);
+    wp_register_style('singleCss', get_template_directory_uri() . '/CSS/single.css', array(), "", false);
+    wp_register_style( 'kontakta-oss', get_template_directory_uri() . '/CSS/kontakta-oss.css', array(), "", false );
+    wp_register_style('policy', get_template_directory_uri() . '/CSS/policy.css', array(), "", false);
+    wp_register_style('varukorg', get_template_directory_uri() . '/CSS/varukorg.css', array(), "", false);
+    wp_register_style('widgetsAndIcons', get_template_directory_uri() . '/CSS/widgetsAndIcons.css', array(), "", false);
+    wp_register_style( 'single-product', get_template_directory_uri() . '/CSS/single-product.css', array(), "", false );
+    wp_register_style( 'archive-product', get_template_directory_uri() . '/CSS/archive-product.css', array(), "", false );
+    wp_register_style('kassaCss', get_template_directory_uri() . '/CSS/kassa.css', array(), "", false);
+    wp_register_style('butiker', get_template_directory_uri() . '/CSS/butiker.css', array(), "", false);
+    
+    wp_enqueue_style( 'commonStyle' );
+    wp_enqueue_style( 'widgetsAndIcons' );
+    
+    if ( basename( $template ) === 'index.php' ) {
+        wp_enqueue_style( 'indexStyle' );
+        wp_enqueue_style( 'animations' );
+        wp_enqueue_script('scripts', get_template_directory_uri() . '/scripts/main.js', array(), '1.0.0', true);
 
-    //    wp_enqueue_style( 'animations', get_template_directory_uri() . './css/animation.css', false );
+    }
+    else if ( basename( $template ) === 'single-shop_page.php' ) {
+        wp_enqueue_style( 'butiker' );
+    }
+    else if ( basename( $template ) === 'archive-product.php' ) {
+        wp_enqueue_style( 'archive-product' );
+    }
+    else if ( basename( $template ) === 'single-product.php' ) {
+        wp_enqueue_style( 'single-product' );
+    }
+    else if ( basename( $template ) === 'home.php' ) {
+        wp_enqueue_style( 'homeCss' );
+    }
+    else if ( basename( $template ) === 'page-kontakta-oss.php' ) {
+        wp_enqueue_style( 'kontakta-oss' );
+    }
+    else if ( basename( $template ) === 'page-mitt-konto.php' ) {
+        wp_enqueue_style( 'styleForMyAcc' );
+    }
+    else if ( basename( $template ) === 'page-kassan.php' ) {
+        wp_enqueue_style( 'kassaCss' );
+    }
+    else if ( basename( $template ) === 'page-varukorg.php' ) {
+        wp_enqueue_style( 'varukorg' );
+    }
+    else if ( basename( $template ) === 'page-integritetspolicy.php' ) {
+        wp_enqueue_style( 'policy' );
+    }
+
 }
 
 
@@ -30,10 +70,6 @@ function custom_override_checkout_fields($fields)
     return $fields;
 }
 
-function load_scripts()
-{
-    wp_enqueue_script('scripts', get_template_directory_uri() . '/scripts/main.js', array(), '1.0.0', true);
-}
 
 //Short code functions
 
@@ -55,9 +91,16 @@ register_sidebar([
 
 
 register_sidebar([
-    'name' => 'footer top',
-    'Description' => 'footer top area',
-    'id' => 'footer_top',
+    'name' => 'footer top left',
+    'Description' => 'footer top left area',
+    'id' => 'footer_top_left',
+    "before_widget" => false,
+]);
+
+register_sidebar([
+    'name' => 'footer top right',
+    'Description' => 'footer top right area',
+    'id' => 'footer_top_right',
     "before_widget" => false,
 ]);
 
@@ -68,14 +111,22 @@ register_sidebar([
     "before_widget" => false,
 ]);
 
+register_sidebar([
+    'name' => 'footer social menu',
+    'Description' => 'footer social menu',
+    'id' => 'footer_social_menu',
+    "before_widget" => false,
+]);
 
-add_action( 'wp_enqueue_scripts', 'load_scripts' );
-add_action( 'wp_enqueue_scripts', 'load_styles' );
+
+
+add_action( 'wp_enqueue_scripts', 'load_styles_and_scripts' );
 add_theme_support('post-thumbnails');
 add_theme_support('menus');
 add_theme_support('woocommerce');
 add_theme_support("widgets");
 add_theme_support('wp-block-styles');
+
 
 // Add support for full and wide align images.
 
