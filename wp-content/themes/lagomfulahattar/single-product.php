ef44d899a6bcb2dc52e245d10bd6f760b53d25aa
Joakim <j-pf@live.se>
2021-05-12 17:59:44 +0200

    styling complete!

--- wp-content/themes/lagomfulahattar/single-product.php
+++ wp-content/themes/lagomfulahattar/single-product.php
@@ -25,14 +25,14 @@ get_header(); ?>
 	<?php
 		do_action( 'woocommerce_before_main_content' );
 	?>
-		<div class="apa">
+	<!-- 	<div class="apa"> -->
 		<?php while ( have_posts() ) : ?>
 			<?php the_post(); ?>
 
 			<?php wc_get_template_part( 'content', 'single-product' ); ?>
 
 		<?php endwhile; // end of the loop. ?>
-		</div>
+	<!-- 	</div> -->
 	<?php
 		do_action( 'woocommerce_after_main_content' );
 	?>
